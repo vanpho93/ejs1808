ba152af64b68811f4094ba7fe745e51930241ed6
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-04 20:10:17 +0700

    ejs

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('home'));
 
+app.get('/learn', (req, res) => res.render('learn'));
+
 app.listen(3000, () => console.log('Server started'));
