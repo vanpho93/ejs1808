9ec8645f302aea5b3406465f38b2f314944cb5c1
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-04 19:59:11 +0700

    htmls

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
+// template engine
 const app = express();
 
-app.get('/', (req, res) => res.send('Xin chao'));
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('home'));
 
 app.listen(3000, () => console.log('Server started'));
