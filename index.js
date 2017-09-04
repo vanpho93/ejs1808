14e05bdfc7b42276be8ddb732c1607f430c9b2cc
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-04 20:33:56 +0700

    auto reload

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const reload = require('reload'); // eslint-disable-line
+
 // template engine
 const app = express();
 
@@ -8,5 +10,7 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('home'));
 
 app.get('/learn', (req, res) => res.render('learn'));
+
+reload(app);
 
 app.listen(3000, () => console.log('Server started'));
